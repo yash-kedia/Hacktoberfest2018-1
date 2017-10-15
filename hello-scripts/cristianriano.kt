81592f4a8084e0ca7952de1637e5e40e13624ced
Cristian Riaño <cristiancarm11@gmail.com>
2017-10-15 22:12:27 +0200

    HelloWorld with Kotlin

---
+++ hello-scripts/cristianriano.kt
@@ -0,0 +1,9 @@
+# LANGUAGE: Kotlin
+# AUTHOR: Cristian Riaño
+# GITHUB: https://github.com/cristianriano
+
+package helloScripts
+
+fun main(args : Array<String>){
+	println("Hi from Kotlin! Don't stop coding!!")
+}
