2b1cb87593297a2e4fe3fe3f783a691d06f54fab
Neil Stewart <neil.stewart@cloudreach.com>
2017-10-16 15:50:35 +0100

    Hacktoberfest Sub

---
+++ hello-scripts/nmyster.rb
@@ -0,0 +1,5 @@
+# LANGUAGE: Ruby
+# AUTHOR: Neil Stewart
+# GITHUB: https://github.com/nmysrer
+
+puts "Hello everyone, Check out my Ruby Sub for this repo #Hacktoberfest!"
---
+++ hello-scripts/nmyster.sh
@@ -0,0 +1,5 @@
+# LANGUAGE: Bash
+# AUTHOR: Neil Stewart
+# GITHUB: https://github.com/nmysrer
+
+echo "Hello everyone, Check out my Bash Sub for this repo #Hacktoberfest!"
