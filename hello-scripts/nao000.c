cedd6d0e956f936974b11d402151ba9a08b70e3d
Nao <wannabeageek0@gmail.com>
2017-10-19 23:21:29 +0900

    add a hello script ver C

---
+++ hello-scripts/nao000.c
@@ -0,0 +1,12 @@
+<?php
+// LANGUAGE: C
+// ENV: C
+// AUTHOR: Naomichi Kubota
+// GITHUB: "https://github.com/Nao000"
+
+
+#include <stdio.h>
+int main(void){
+    printf("hello world");
+    return 0;
+}
