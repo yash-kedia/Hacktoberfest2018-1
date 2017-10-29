b3cfd01dd725c9ff1c2c093b98a8c83fc921f63d
Lucas Souza <lucasdsdc@gmail.com>
2017-10-29 17:46:23 -0200

    Added Hello World in Kotlin

---
+++ hello-scripts/lucaszeta.kt
@@ -0,0 +1,11 @@
+/**
+ * LANGUAGE: Kotlin
+ * AUTHOR: Lucas Souza
+ * GITHUB: https://github.com/lucaszeta
+ */
+
+package Hacktoberfest2017.hello_scripts 
+ 
+fun main(args : Array<String>) {
+    println("Hello, world!")
+}
